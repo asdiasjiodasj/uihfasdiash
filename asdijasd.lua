8e610df33f7a303a63ab69cf25735cf8a60873a0
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-10-03 13:56:59 +0200

    Update asdijasd.lua

--- asdijasd.lua
+++ asdijasd.lua
@@ -5,10 +5,7 @@ whitelist = {
 
 if syn then
     if whitelist[game.Players.LocalPlayer.Name] then
-        if not game.CoreGui["Project Anti Abusers"] then
             loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/asdiasjiodasj/uihfasdiash/main/asdsdfgdfdfgdfgfasd.lua', true))()
-        else end
-    else game:Shutdown()
     end
 else game:Shutdown()
 end
